8763b8168cd4f88893c994097d686fde1cfcb637
RichForever <mateuszmisiak1@gmail.com>
2023-03-21 21:34:23 +0100

    commit

---
+++ theme/functions.php
@@ -0,0 +1,233 @@
+<?php
+
+/**
+ * @package WordPress
+ * @subpackage Timberland
+ * @since Timberland 1.1.0
+ */
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Timber\Menu;
+use Timber\Post;
+use Timber\Site;
+use Timber\Timber;
+use Timber\PostQuery;
+
+$timber = new Timber();
+
+Timber::$dirname = array('views', 'blocks');
+
+class Timberland extends Site
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+        add_action('after_setup_theme', [$this, 'theme_supports']);
+        add_filter('timber/context', [$this, 'add_to_context']);
+        add_filter('timber/twig', [$this, 'add_to_twig']);
+        add_action('init', [$this, 'register_custom_post_types']);
+        add_action('init', [$this, 'register_taxonomies']);
+        add_action('block_categories_all', [$this, 'block_categories_all']);
+//        add_action('acf/init', [$this, 'acf_register_blocks']);
+        //add_filter('allowed_block_types', [$this, 'allowed_block_types']);
+        add_action('enqueue_block_editor_assets', [$this, 'enqueue_block_editor_assets']);
+
+        parent::__construct();
+    }
+
+    public function add_to_context($context)
+    {
+        $context['site'] = $this;
+        $context['menu'] = new Menu();
+        
+        // Require block functions files
+        foreach (glob(dirname(__FILE__) . "/blocks/*/functions.php") as $file) {
+            require_once $file;
+        }
+
+        return $context;
+    }
+
+    public function add_to_twig($twig)
+    {
+        return $twig;
+    }
+
+    public function theme_supports()
+    {
+        add_theme_support('automatic-feed-links');
+        add_theme_support(
+            'html5',
+            [
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption'
+            ]
+        );
+        add_theme_support('menus');
+        add_theme_support('post-thumbnails');
+        add_theme_support('title-tag');
+        add_theme_support('editor-styles');
+        add_editor_style('public/editor-style.css');
+    }
+
+    public function enqueue_scripts()
+    {
+        wp_dequeue_style('wp-block-library');
+        wp_dequeue_style('wp-block-library-theme');
+        wp_dequeue_style('wc-block-style');
+        wp_dequeue_script('jquery');
+
+        $mixPublicPath = get_template_directory() . '/public';
+
+        wp_enqueue_style('style', get_template_directory_uri() . '/public/' . $this->mix("styles.css", $mixPublicPath));
+        wp_enqueue_script('app', get_template_directory_uri() . '/public/' . $this->mix("scripts.js", $mixPublicPath), array(), '', true);
+    }
+
+    public function register_custom_post_types()
+    {
+        // Example
+        // $labels = [
+        //     "name" => "Example",
+        //     "singular_name" => "Example",
+        // ];
+
+        // $args = [
+        //     "label" => "Example",
+        //     "labels" => $labels,
+        //     "description" => "",
+        //     "public" => true,
+        //     "publicly_queryable" => true,
+        //     "show_ui" => true,
+        //     "show_in_rest" => true,
+        //     "rest_base" => "",
+        //     "rest_controller_class" => "WP_REST_Posts_Controller",
+        //     "has_archive" => false,
+        //     "show_in_menu" => true,
+        //     "show_in_nav_menus" => true,
+        //     "delete_with_user" => false,
+        //     "exclude_from_search" => false,
+        //     "capability_type" => "post",
+        //     "map_meta_cap" => true,
+        //     "hierarchical" => false,
+        //     "rewrite" => ["slug" => "example", "with_front" => true],
+        //     "query_var" => true,
+        //     "menu_icon" => "dashicons-groups",
+        //     "supports" => ["title"],
+        // ];
+
+        // register_post_type("example", $args);
+    }
+
+    public function register_taxonomies()
+    {
+        // Example
+        // $labels = [
+        //     "name" => "Example",
+        //     "singular_name" => "Example",
+        // ];
+
+        // $args = [
+        //     "label" => "Example",
+        //     "labels" => $labels,
+        //     "public" => true,
+        //     "publicly_queryable" => true,
+        //     "hierarchical" => false,
+        //     "show_ui" => true,
+        //     "show_in_menu" => true,
+        //     "show_in_nav_menus" => true,
+        //     "query_var" => true,
+        //     "rewrite" => ['slug' => 'example', 'with_front' => true,],
+        //     "show_admin_column" => false,
+        //     "show_in_rest" => true,
+        //     "rest_base" => "example",
+        //     "rest_controller_class" => "WP_REST_Terms_Controller",
+        //     "show_in_quick_edit" => false,
+        // ];
+
+        // register_taxonomy("example", ["posttype"], $args);
+    }
+
+    public function block_categories_all($categories) {
+        return array_merge([['slug' => 'custom', 'title' => __('Custom')]], $categories);
+    }
+
+//    public function acf_register_blocks() {
+//        foreach (new DirectoryIterator(dirname(__FILE__) . '/blocks') as $dir) {
+//            if ($dir->isDot()) continue;
+//
+//            $settings = [
+//                'name' => $dir->getFilename(),
+//                'title' => __(ucwords(str_replace('-', ' ', $dir->getFilename()))),
+//                'description' => __(''),
+//                'render_callback' => [$this, 'acf_block_render_callback'],
+//                'category' => 'custom',
+//                'align' => false,
+//                'example'  => array(
+//                    'attributes' => array(
+//                        'mode' => 'preview',
+//                        'data' => array()
+//                    )
+//                )
+//            ];
+//
+//            acf_register_block_type($settings);
+//        }
+//    }
+
+    public function acf_block_render_callback( $block, $content = '', $is_preview = false ) {
+        $context = Timber::context();
+        $context['block'] = $block;
+        $context['fields'] = get_fields();
+        $context['is_preview'] = $is_preview;
+        $dirName = str_replace('acf/', '', $block['name']);
+    
+        Timber::render('blocks/'. $dirName . '/index.twig', $context);
+    }
+
+    public function allowed_block_types() {
+        $allowed_blocks = [
+            'core/columns'
+        ];
+
+        foreach (new DirectoryIterator(dirname(__FILE__) . '/blocks') as $dir) {
+            $allowed_blocks[] = 'acf/' . $dir;
+        }
+
+        return $allowed_blocks;
+    }
+
+    public function enqueue_block_editor_assets() {
+        //wp_enqueue_style('prefix-editor-font', '//fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap');
+        wp_enqueue_script('app', get_template_directory_uri() . '/assets/build/app.js');
+    }
+
+    public function mix($path, $manifestDirectory)
+    {
+        static $manifest;
+
+        if (! $manifest) {
+            if (! file_exists($manifestPath = $manifestDirectory.'/manifest.json')) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        if (strpos($path, '/') !== 0) {
+            $path = "{$path}";
+        }
+
+
+        if (! array_key_exists($path, $manifest)) {
+            throw new Exception(
+                "Unable to locate Mix file: {$path}. Please check your webpack.mix.js output paths and try again."
+            );
+        }
+//
+        return $manifest[$path];
+    }
+}
+
+new Timberland();
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+module.exports = {
+  mode: isProduction ? "production" : "development",
+  entry: {
+    "scripts": "./theme/src/scripts/app.js",
+    "styles": "./theme/src/styles/app.scss",
+    "editor-styles": "./theme/src/styles/editor-styles.scss",
+  },
+  // entry: "./theme/src/scripts/app.js",
+  output: {
+    path: path.resolve(__dirname, "theme/public"),
+    filename: isProduction ? "[name].[contenthash:8].js" : "[name].js",
+    chunkFilename: isProduction ? "[id].[contenthash:8].js" : "[id].js",
+    clean: true,
+  },
+  plugins: [
+    new WebpackAssetsManifest({
+      output: path.resolve(__dirname, "theme/public/manifest.json"),
+      customize(entry, original, manifest, asset) {
+        const pattern = /\.(js|css)$/i;
+        if (!pattern.test(entry.key)) {
+          return false;
+        }
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? "[name].[contenthash:8].css" : "[name].css",
+      chunkFilename: isProduction ? "[id].[contenthash:8].css" : "[id].css",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    alias: {
+      images: path.join(__dirname, "theme/src/images"),
+      fonts: path.join(__dirname, "theme/src/fonts"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        severityError: "warning",
+        minimizer: {
+          //   filename: "[name][ext]",
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-mozjpeg",
+              "imagemin-pngquant",
+              "imagemin-svgo",
+            ],
+          },
+        },
+      }),
+    ],
+  },
+  devtool: "source-map",
+};
