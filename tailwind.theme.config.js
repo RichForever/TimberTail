9d0eb12c28f7ffe4f4b332fc247d26019f012149
RichForever <mateuszmisiak1@gmail.com>
2023-04-18 21:58:56 +0200

    add inter font

--- tailwind.theme.config.js
+++ tailwind.theme.config.js
@@ -114,9 +114,6 @@ const theme = {
       '7xl': '3.8125rem',
       '8xl': '4rem'
   },
-  fontFamily: {
-      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
-  },
   borderRadius: {
       none: '0',
       xs: '0.125rem',
