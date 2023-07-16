c10de096c59c3f52710deee837dc1de4eb188da2
RichForever <mateuszmisiak1@gmail.com>
2023-07-16 11:44:49 +0200

    remove old code

--- theme/lib/blocks-register.php
+++ theme/lib/blocks-register.php
@@ -14,10 +14,3 @@ asort($blocks);
 foreach($blocks as $block) {
 	register_block_type($block);
 }
-
-
-//foreach (new DirectoryIterator(dirname(__FILE__) . '/blocks') as $dir) {
-//	if ($dir->isDot()) continue;
-//
-//	register_block_type( dirname(__FILE__) . '/blocks/' . $dir->getFilename() . '/block.json' );
-//}
