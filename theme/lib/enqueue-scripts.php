5824635290851968da6ab4a052f91446afa811f8
RichForever <mateuszmisiak1@gmail.com>
2023-08-03 17:34:20 +0200

    fix jquery

--- theme/lib/enqueue-scripts.php
+++ theme/lib/enqueue-scripts.php
@@ -4,7 +4,9 @@ require_once "manifest-mix.php";
 wp_dequeue_style('wp-block-library');
 wp_dequeue_style('wp-block-library-theme');
 wp_dequeue_style('wc-block-style');
-wp_dequeue_script('jquery');
+if (!is_admin()) {
+	wp_dequeue_script( 'jquery' );
+}
 
 $mixPublicPath = get_template_directory() . '/dist';
 
