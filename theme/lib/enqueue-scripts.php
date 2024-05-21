b8ca422dc421639bd4694d28ee2a3ae31080283b
RichForever <mateuszmisiak1@gmail.com>
2024-05-21 14:08:41 +0200

    Update enqueue-scripts.php

--- theme/lib/enqueue-scripts.php
+++ theme/lib/enqueue-scripts.php
@@ -10,9 +10,9 @@ if (!is_admin()) {
 
 $mixPublicPath = get_template_directory() . '/dist';
 
-wp_enqueue_style('style', get_template_directory_uri() . '/dist/' . mix("styles.css", $mixPublicPath));
+wp_enqueue_style('style', get_template_directory_uri() . '/dist/' . mix("app-styles.css", $mixPublicPath));
 wp_enqueue_style('theme', get_template_directory_uri() . '/dist/' . mix("theme.css", $mixPublicPath));
-wp_enqueue_script('app', get_template_directory_uri() . '/dist/' . mix("scripts.js", $mixPublicPath), array(), '', true);
+wp_enqueue_script('app', get_template_directory_uri() . '/dist/' . mix("app-scripts.js", $mixPublicPath), array(), '', true);
 wp_localize_script( 'app', 'wp', array(
     'siteUrl' => home_url()
 ));
