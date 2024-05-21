e3a4aec330aa64a417282313f6144b6a5624c7ed
RichForever <mateuszmisiak1@gmail.com>
2024-05-21 14:10:57 +0200

    Update theme-supports.php

--- theme/lib/theme-supports.php
+++ theme/lib/theme-supports.php
@@ -18,4 +18,18 @@ add_theme_support('editor-styles');
 
 // register editor styles
 $mixPublicPath = get_template_directory() . '/dist';
-add_editor_style(get_template_directory_uri() . '/dist/' . mix("editor.css", $mixPublicPath));
+add_editor_style(get_template_directory_uri() . '/dist/' . mix("editor-styles.css", $mixPublicPath));
+
+function enqueue_theme_editor_custom_assets() {
+    $template_slug = get_page_template_slug();
+    if('page-content.php' === $template_slug) {
+        $mixPublicPath = get_template_directory() . '/dist';
+        wp_enqueue_script('editor-scripts-js', get_template_directory_uri() . '/dist/' . mix("editor-scripts.js", $mixPublicPath), [
+            'wp-blocks',
+            'wp-element',
+            'wp-components',
+            'wp-i18n'
+        ]);
+    }
+}
+add_action('enqueue_block_editor_assets', 'enqueue_theme_editor_custom_assets');
